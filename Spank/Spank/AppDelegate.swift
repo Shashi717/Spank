a6c443e0c2e4e432efcb761ef4015df5f7c15c1b
Shashi <liyanagemadushani21@gmail.com>
2017-12-27 22:23:44 -0500

    changes

--- Spank/Spank/AppDelegate.swift
+++ Spank/Spank/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FIRApp.configure()
+        STPPaymentConfiguration.shared().publishableKey = "pk_test_eGC2busZYl4A5pLZIr0IxHpj"
         return true
     }
 
